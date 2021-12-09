025d33db8c851f4d0fd5b65f1647fc479a6964ef
NathanNuckels <nathannuckels@gmail.com>
2021-12-09 02:29:15 +0000

    modified:   backend.py

--- backend.py
+++ backend.py
@@ -1,4 +1,27 @@
 inport os
+
+ckass Filesystem:
+	def __init__(self,folder):
+		self.dir=folder
+		self.folders=[]
+		self.files=[]
+		if not os.path.exists(self.dir):
+			os.makedirs(self.dir)
+		os.chdir(os.path.join(os.getcwd(),self.dir))
+		if not len(os.listdir(os.getcwd())):
+			self.index()
+		#...
+	
+	def index(self):
+		ls=os.listdir(os.getcwd())
+		for discriptor in ls:
+			if os.path.isdir():
+				self.folders.append(Filesystem(discriptor))
+			elif os.path.isfile(discriptor):
+				self.files.append(discriptor)
+			else:
+				print("Unknown type:"+discriptor)
+'''
 def format(dir):
 	if not os.path.exists(dir):
 		os.makedirs(dir)
@@ -12,3 +35,4 @@ def format(dir):
 def refresh(dir):
 	blocks=os.listdir(dir)
 	
+'''
