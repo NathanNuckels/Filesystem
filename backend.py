73a092c021d0527d240ac59d9e3618a93a28f83d
NathanNuckels <48299408+NathanNuckels@users.noreply.github.com>
2021-12-10 19:15:24 -0600

    Made corrections and improved indexing

--- backend.py
+++ backend.py
@@ -1,26 +1,38 @@
-inport os
+import os
 
-ckass Filesystem:
-	def __init__(self,folder):
+class Filesystem:
+	def __init__(self,folder,disp=True,root=True):
 		self.dir=folder
+		self.displayDir=folder
 		self.folders=[]
 		self.files=[]
+		self.subSystems=[]
+		self.root=root
 		if not os.path.exists(self.dir):
 			os.makedirs(self.dir)
 		os.chdir(os.path.join(os.getcwd(),self.dir))
-		if not len(os.listdir(os.getcwd())):
+		if len(os.listdir(os.getcwd()))==0:
 			self.index()
-		#...
-	
-	def index(self):
+		if disp:
+			print("[FS] ["+self.dir+"] Created")
+		
+		
+	def index(self,recursive=True):
+		print("[FS] ["+self.dir+"] Indexing...")
 		ls=os.listdir(os.getcwd())
 		for discriptor in ls:
 			if os.path.isdir():
 				self.folders.append(Filesystem(discriptor))
+				self.subSystems.append(Filesystem(discriptor,False))
+				self.subSystems[len(self.subSystems)-1].displayDir = self.dir + "/" + self.subSystems[len(self.subSystems)-1].dir
+				print("[FS] ["+self.dir+"/"+discriptor+"] Created")
+				self.subSystems[len(self.Subsystems)-1].index()
 			elif os.path.isfile(discriptor):
 				self.files.append(discriptor)
 			else:
-				print("Unknown type:"+discriptor)
+				print("[FS] Unknown type:"+discriptor)
+	def generateKey(self):
+		pass
 '''
 def format(dir):
 	if not os.path.exists(dir):
@@ -36,3 +48,5 @@ def refresh(dir):
 	blocks=os.listdir(dir)
 	
 '''
+fs=Filesystem("testFolder")
+fs.index()
